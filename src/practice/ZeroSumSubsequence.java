e154cb5f845ba7aa4b4a16cc99dcc60d251a7e6c
Peter Liska <liska.peto@gmail.com>
2020-10-04 08:54:53 +0200

    Clean solution commit.

--- src/practice/ZeroSumSubsequence.java
+++ src/practice/ZeroSumSubsequence.java
@@ -1,8 +1,5 @@
 package practice;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -33,39 +30,4 @@ public class ZeroSumSubsequence {
 		return false;
 	}
 
-	public static void main(String[] args) {
-
-		try {
-			PrintWriter writer = new PrintWriter("test.out", "UTF-8");
-			BufferedReader reader = new BufferedReader(new FileReader("test.in"));
-			ZeroSumSubsequence zeroSumSubsequence = new ZeroSumSubsequence();
-
-			String line = reader.readLine();
-			int lineNum = Integer.parseInt(line);
-
-			for (int k = 0; k < lineNum; k++) {
-				line = reader.readLine();
-				int size = Integer.parseInt(line);
-				int arr[] = new int[size];
-
-				line = reader.readLine();
-				String[] strArr = line.split(" ");
-				for (int i = 0; i < strArr.length; i++) {
-					arr[i] = Integer.parseInt(strArr[i]);
-				}
-
-				if (zeroSumSubsequence.solution(arr)) {
-					writer.println("yes");
-				} else {
-					writer.println("no");
-				}
-			}
-
-			reader.close();
-			writer.close();
-		} catch (Exception ex) {
-			System.out.println("Problem when reading file!");
-		}
-
-	}
 }

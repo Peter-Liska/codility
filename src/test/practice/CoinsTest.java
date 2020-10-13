d2c419677ef48c30186207127bc88b3a51c80806
Peter Liska <liska.peto@gmail.com>
2020-10-13 20:18:19 +0200

    New tests added.

--- src/test/practice/CoinsTest.java
+++ src/test/practice/CoinsTest.java
@@ -39,4 +39,59 @@ class CoinsTest {
 		Assertions.assertEquals(true, coins.solution(30));
 	}
 
+	@Test
+	void test5308() {
+		Assertions.assertEquals(true, coins.solution(5308));
+	}
+
+	@Test
+	void test718() {
+		Assertions.assertEquals(true, coins.solution(718));
+	}
+
+	@Test
+	void test3302() {
+		Assertions.assertEquals(false, coins.solution(3302));
+	}
+
+	@Test
+	void test923() {
+		Assertions.assertEquals(false, coins.solution(923));
+	}
+
+	@Test
+	void test3400() {
+		Assertions.assertEquals(false, coins.solution(3400));
+	}
+
+	@Test
+	void test3189() {
+		Assertions.assertEquals(false, coins.solution(3189));
+	}
+
+	@Test
+	void test204() {
+		Assertions.assertEquals(false, coins.solution(204));
+	}
+
+	@Test
+	void test6898() {
+		Assertions.assertEquals(true, coins.solution(6898));
+	}
+
+	@Test
+	void test8983() {
+		Assertions.assertEquals(true, coins.solution(8983));
+	}
+
+	@Test
+	void test386() {
+		Assertions.assertEquals(true, coins.solution(386));
+	}
+
+	@Test
+	void test4805() {
+		Assertions.assertEquals(true, coins.solution(4805));
+	}
+
 }

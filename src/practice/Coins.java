c632a9ada4ef12c46a80868b0dec1988c4d6ec91
Peter Liska <liska.peto@gmail.com>
2020-10-13 20:08:51 +0200

    Small improvements.

--- src/practice/Coins.java
+++ src/practice/Coins.java
@@ -7,23 +7,27 @@ import java.io.PrintWriter;
 public class Coins {
 
 	public boolean solution(int coins) {
+
+		if (coins == 0) return false;
 		if (coins == 1) return true;
 		if (coins == 2) return false;
 		if (coins == 3) return true;
 
 		boolean[] win = new boolean[coins + 1];
+		win[0] = true;
+		win[1] = true;
+		win[2] = false;
+		win[3] = true;
 
-		for (int i = 1; i <= coins; i++) {
-
+		for (int i = 4; i <= coins; i++) {
 			for (int k = 1; k * k <= i; k++) {
 
-				if (!win[i - (k * k)]) {
+				int square = k * k;
+				if (i - square == 0 || !win[i - square]) {
 					win[i] = true;
 					break;
 				}
-
 			}
-
 		}
 
 		return win[coins];
